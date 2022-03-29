e6f1ec09c9f2a35c21cb8934fe40dbc0356768ae
Joyejin <99413189+hodurang@users.noreply.github.com>
2022-03-29 20:14:19 +0900

    Revert ""시작화면/점수보드/게임종료/키보드이동 구현""

--- TetrisGame/src/tetris/Tetris.java
+++ TetrisGame/src/tetris/Tetris.java
@@ -1,40 +1,11 @@
 package tetris;
 
-import javax.swing.JOptionPane;
-
 public class Tetris {
-	private static GameForm gf;
-	private static StartupForm sf;
-	private static LeaderboardForm lf;
-	
-	public static void start() {
-		gf.setVisible(true);
-		gf.startGame();
-	}
-	
-	public static void showLeaderboard() {
-		lf.setVisible(true);
-	}
-
-	public static void showStartup() {
-		sf.setVisible(true);
-	}
-	
-	public static void gameOver(int score) {
-		String playerName = JOptionPane.showInputDialog("Gane Over!\n Please enter your name.");
-		gf.setVisible(false);
-		lf.addPlayer(playerName, score);
-	}
 
 	public static void main(String[] args) {
-		java.awt.EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				sf = new StartupForm();
-				gf = new GameForm();
-				lf = new LeaderboardForm();
-				
-				sf.setVisible(true);
-			}
-		});
+		// 동작 확인을 위해 생성, 나중에 수정필요
+		GameForm gf = new GameForm();
+		gf.setVisible(true);
 	}
+
 }
